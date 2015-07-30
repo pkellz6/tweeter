c5eeaaa8e1b222e9375c3ed5cababb4253e28cc8
Pat Kelly <pkellz6@me.com>
2015-07-30 12:06:30 -0400

    make routes prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'statuses/index'
+  get 'statuses',
+  to: 'statuses#index',
+  page: 1
+  
+  get 'statuses/page/:page',
+  to: 'statuses#index'
 
   get 'statuses/show'
 
   get 'statuses/new'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
